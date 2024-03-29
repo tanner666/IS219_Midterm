5d24b05494eeb7a1cad9a7aeeb2824c869dd4b72
tanner666 <tjg5@njit.edu>
2024-03-29 15:43:27 -0400

    initial file setup

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+from calculator import Calculator    
+
+# You must put this in your main.py because this forces the program to start when you run it from the command line.
+if __name__ == "__main__":
+    calculator = Calculator().start()  # Instantiate an instance of App
+    
