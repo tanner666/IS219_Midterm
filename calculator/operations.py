49ace8e8d739a109ce72971aa15f6849d387734b
tanner666 <tjg5@njit.edu>
2024-03-29 22:03:27 -0400

    updated divide operation

--- calculator/operations.py
+++ calculator/operations.py
@@ -14,10 +14,11 @@ def multiply(a: Decimal, b: Decimal) -> Decimal:
     return a * b
 
 def divide(a: Decimal, b: Decimal) -> Decimal:
-    if b == 0:
-        logging.warning("Cannot divide by zero!")
+    try:
+        return a/b
+    except ZeroDivisionError:
+        logging.warning("Cannot divide by zero")
         return None
-    return a / b
 
 def log(a: Decimal, base: Decimal = Decimal('10')) -> Decimal:
     """Default log for base 10"""
